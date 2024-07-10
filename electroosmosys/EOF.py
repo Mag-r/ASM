bc61a4c972dd252f778a7694d0be5ebfa7f61cd1
Marcel Gern <marcel@familie-gern.de>
2024-07-10 11:58:34 +0200

    added external field and tracking of flux density

--- electroosmosys/EOF.py
+++ electroosmosys/EOF.py
@@ -3,6 +3,7 @@ import numpy as np
 import espressomd.io.writer.vtf as vtf
 import espressomd.shapes
 import espressomd.lb
+import espressomd.observables as obs
 from tqdm import tqdm
 
 
@@ -16,6 +17,7 @@ N_particles_per_wall_per_dim = 20
 fluid_density = 26.18
 viscosity = 0.25
 friction = 15.0
+E_field = [25.0,0,0]
 
 system = espressomd.System(box_l=[box_length, box_length, box_length+2*safe_margin+elc_gap])
 system.time_step = 0.01
@@ -55,7 +57,7 @@ for i in range(N_particles_per_wall_per_dim):
             fix=[True, True, True]
         )
 
-# Add Walls
+# Add Walls 
 wall = espressomd.shapes.Wall(normal=[0, 0, 1], dist=0)
 system.constraints.add(shape=wall, particle_type=1)
 wall = espressomd.shapes.Wall(normal=[0, 0, -1], dist=-(box_length+elc_gap+safe_margin))
@@ -65,11 +67,15 @@ system.constraints.add(shape=wall, particle_type=1)
 system.non_bonded_inter[0, 0].wca.set_params(epsilon=1, sigma=1)
 system.non_bonded_inter[0, 1].wca.set_params(epsilon=1, sigma=1)
 
-
+# Add external E-field
+E_field = espressomd.constraints.LinearElectricPotential(E=E_field)
+system.constraints.add(E_field)
 
 # Write initial configuration
 vtf.writevsf(system, fp)
 vtf.writevcf(system, fp)
+
+part_vel = obs.FluxDensityProfile(ids=range(num_free_particles), n_x_bins =1,n_y_bins=1,n_z_bins=100,min_x=0,max_x=box_length,min_y=0,max_y=box_length,min_z=safe_margin,max_z=box_length+2*safe_margin)
 
 print("Cooldown")
 system.integrator.set_steepest_descent(f_max=0, gamma=0.1, max_displacement=0.1)
@@ -82,10 +88,17 @@ solver = espressomd.electrostatics.P3M(prefactor=bjerrum_length, accuracy=1e-3)
 elc = espressomd.electrostatics.ELC(actor=solver, gap_size=elc_gap, maxPWerror=1e-2)
 system.electrostatics.solver = elc
 
-print("Run")
+print("Equilibrate")
 system.integrator.set_vv()
 system.thermostat.set_lb(LB_fluid=lbf, seed=42,gamma = friction)
+system.integrator.run(1000)
+
+print("Run")
+list=[]
 for i in tqdm(range(1000)):
     system.integrator.run(100)
+    list.append(part_vel.calculate())
     vtf.writevcf(system, fp)
 fp.close()
+list=np.array(list)
+np.save("flux_density.npy",list)
